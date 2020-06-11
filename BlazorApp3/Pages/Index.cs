dc7b3af8292b25955453e607fca4bd1297a42f98
Meir Kriheli <meir.kriheli@hp.com>
2020-06-11 16:21:53 +0300

    use partial class

---
+++ BlazorApp3/Pages/Index.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reactive.Linq;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp3.Pages
+{
+    public partial class Index : IDisposable
+    {
+        private string _timeAppIsUp;
+        private IDisposable _disposable;
+
+        [Inject]
+        private IStopWatchService StopWatchService { get; set; }
+
+        #region Overrides of ComponentBase
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            _disposable = StopWatchService.GetTimeFromStart()
+                .Select(ts => ts.ToString())
+                .Subscribe(s =>
+                {
+                    Console.WriteLine($"Arrived: {s}");
+                    _timeAppIsUp = s;
+                    base.StateHasChanged();
+                });
+        }
+
+        #endregion
+
+
+        #region Implementation of IDisposable
+
+        public void Dispose()
+        {
+            _disposable?.Dispose();
+        }
+
+        #endregion
+    }
+}
